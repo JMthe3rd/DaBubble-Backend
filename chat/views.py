4c5d7d6275b8d78e34095f938af87f3eae153c3e
Julius Marecek <jules.marecek@gmail.com>
2024-01-16 15:14:46 +0100

    added Frontend ApiCalls as Comments to the Views / Insert flag to Post-Body

--- chat/views.py
+++ chat/views.py
@@ -26,6 +26,55 @@ class LoginView(ObtainAuthToken):
                 'user_id': user.pk,
                 'email': user.email
             })
+        
+#Frontend ApiCall - Login:
+    # async login(username: string, password: string) {
+   
+    #     try {
+    #     let resp: any = await this.loginWithUsernameAndPassword(username, password);
+    #     console.log(resp);
+    #     localStorage.setItem('token', resp['token']);
+    #     this.router.navigate(['summary']);
+    #     } catch(e) {
+    #     console.error(e);
+    #     }
+    # }
+
+    # loginWithUsernameAndPassword(username: string, password: string) {
+    #     const url = environment.baseURL + "/login/";
+    #     const body = {
+    #       "username": username,
+    #       "password": password
+    #     };
+    #     return lastValueFrom(this.http.post(url, body));
+    # }
+        
+#Frontend Service Interceptor - Token Header: 
+# export class AuthInterceptorService implements HttpInterceptor{
+
+#     constructor(private router: Router) { }
+
+#     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+#         const token = localStorage.getItem('token');
+
+#         if (token) {
+#         request = request.clone({
+#             setHeaders: {Authorization: `Token ${token}`}
+#         });
+#         }
+
+#         return next.handle(request).pipe(
+#         catchError((err) => {
+#             if (err instanceof HttpErrorResponse) {
+#             if (err.status === 401) {
+#                 this.router.navigateByUrl('/login');
+#             }
+#             }
+#             return throwError( () => err);
+#           })
+#         );
+#       }
+#     }
 
 
 class create_User(APIView):
@@ -36,7 +85,18 @@ class create_User(APIView):
                 return Response({'success': 'Benutzer erfolgreich erstellt.'}, status=status.HTTP_201_CREATED)
             else :
                 return Response({'success': 'Passwörter stimmen nicht überein.'})
-            
+
+#Frontend Api Call - User create:
+    # createUser(username: string, email: string, password: string, password_repeat: string  or UserModelClass!) {
+    #     const url = environment.baseURL + "/createuser/";
+    #     const body = {
+    #       "username": username,
+    #       "email": email,
+    #       "password": password,
+    #       "password_repeat": password_repeat
+    #     };
+    #     return lastValueFrom(this.http.post(url, body));
+    # }            
 
 class logout_view(APIView):
       authentication_classes = [TokenAuthentication]
@@ -50,53 +110,127 @@ class logout_view(APIView):
             return Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+#Frontend Api Call - User Logout: 
+    # logout() {
+    #     const url = environment.baseURL + "/logout/";
+    #     return lastValueFrom(this.http.post(url));
+    #     }
 
 class RoomMessage_View(APIView):
       authentication_classes = [TokenAuthentication]
       permission_classes = [IsAuthenticated]
 
-      def get(self, request, format=None):
-            messages = Message.objects.filter(room=request.data['room'])
-            serializer = MessageSerializer(messages, many=True)
-            return Response(serializer.data)
-      
-      def post(self, request, format=None): 
-            room_id = request.data.get('room')
-            room = get_object_or_404(Room, pk=room_id)
-            message = Message.objects.create(sender=request.user, message=request.data['message'], room=room)
-            message.save()
-            return Response({'success': 'Nachricht erfolgreich an Raum abgeschickt.'}, status=status.HTTP_201_CREATED)
+      def post(self, request, format=None):
+            match request.data['methode']:
+                case "get":
+                    messages = Message.objects.filter(room=request.data['room'])
+                    serializer = MessageSerializer(messages, many=True)
+                    return Response(serializer.data)    
+                 
+                case "post":
+                    room_id = request.data.get('room')
+                    room = get_object_or_404(Room, pk=room_id)
+                    message = Message.objects.create(sender=request.user, message=request.data['message'], room=room)
+                    message.save()
+                    return Response({'success': 'Nachricht erfolgreich an Raum abgeschickt.'}, status=status.HTTP_201_CREATED)
+
+#Frontend Api Call - Message get:
+    # getRoomMessages(room: number) {
+    #     const url = environment.baseURL + "/roommessage/";
+    #     const body = {
+    #       "room": room,
+    #       "methode": "get"
+    #     };
+    #     return lastValueFrom(this.http.post(url, body));
+    # } 
+                
+#Frontend Api Call - Message create:
+    # sendRoomMessage(room: number, message: string) {
+    #     const url = environment.baseURL + "/roommessage/";
+    #     const body = {
+    #       "message": message,
+    #       "room": room,
+    #       "methode": "post"
+    #     };
+    #     return lastValueFrom(this.http.post(url, body));
+    # } 
+
       
 class DirectMessage_View(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, format=None):
-            messages = Message.objects.filter(sender=request.user)
-            serializer = MessageSerializer(messages, many=True)
-            return Response(serializer.data)
+    def post(self, request, format=None):
+        match request.data['methode']:
+            case "get":
+                messages = Message.objects.filter(sender=request.user)
+                serializer = MessageSerializer(messages, many=True)
+                return Response(serializer.data)
     
-    def post(self, request, format=None): 
-              reciever_id = request.data.get('reciever')
-              reciever = get_object_or_404(User, pk=reciever_id)
-              message = Message.objects.create(sender=request.user, message=request.data['message'], reciever=reciever)
-              message.save()
-              return Response({'success': 'Nachricht erfolgreich abgeschickt.'}, status=status.HTTP_201_CREATED)
+            case "post":
+                reciever_id = request.data.get('reciever')
+                reciever = get_object_or_404(User, pk=reciever_id)
+                message = Message.objects.create(sender=request.user, message=request.data['message'], reciever=reciever)
+                message.save()
+                return Response({'success': 'Nachricht erfolgreich abgeschickt.'}, status=status.HTTP_201_CREATED)
 
+#Frontend Api Call - Message get:
+    # getMessages(reciever: number) {
+    #     const url = environment.baseURL + "/direct/";
+    #     const body = {
+    #       "reciever": reciever,
+    #       "methode": "get"
+    #     };
+    #     return lastValueFrom(this.http.post(url, body));
+    # } 
+                
+#Frontend Api Call - Message create:
+    # sendMessage(reciever: number, message: string) {
+    #     const url = environment.baseURL + "/direct/";
+    #     const body = {
+    #       "message": message,
+    #       "reciever": reciever,
+    #       "methode": "post"
+    #     };
+    #     return lastValueFrom(this.http.post(url, body));
+    # } 
       
 class Room_View(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, format=None):
-        rooms = Room.objects.all()
-        serializer = RoomSerializer(rooms, many=True)
-        return Response(serializer.data)
+    def post(self, request, format=None):
+        match request.data['methode']:
+            case "get":
+                rooms = Room.objects.all()
+                serializer = RoomSerializer(rooms, many=True)
+                return Response(serializer.data)
     
-    def post(sefl, request, format=None):
-        try:
-            room = Room.objects.create(roomName=request.data['roomName'], description=request.data['description'], created_by=request.user)
-            room.save()
-            return Response({'success': 'Raum erfolgreich erstellt.'}, status=status.HTTP_201_CREATED)
-        except:
-            return Response({'success': 'Fehler'})
+            case "post":
+                try:
+                    room = Room.objects.create(roomName=request.data['roomName'], description=request.data['description'], created_by=request.user)
+                    room.save()
+                    return Response({'success': 'Raum erfolgreich erstellt.'}, status=status.HTTP_201_CREATED)
+                except:
+                    return Response({'success': 'Fehler'})
+                
+#Frontend Api Call - Rooms get:
+    # getRooms(reciever: number) {
+    #     const url = environment.baseURL + "/direct/";
+    #     const body = {
+    #       "methode": "get"
+    #     };
+    #     return lastValueFrom(this.http.post(url, body));
+    # } 
+                
+#Frontend Api Call - Message create:
+    # createRoom(roomName: string, description: string) {
+    #     const url = environment.baseURL + "/direct/";
+    #     const body = {
+    #       "roomName": roomName,
+    #       "description": description,
+    #       "methode": "post"
+    #     };
+    #     return lastValueFrom(this.http.post(url, body));
+    # } 
